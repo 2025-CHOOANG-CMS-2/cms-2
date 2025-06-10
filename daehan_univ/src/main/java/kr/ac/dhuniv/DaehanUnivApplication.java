b64006a0e11eb068ed2d8bdef764f0161f9020bd
HwangInHo1217 <hih1217@skuniv.ac.kr>
2025-06-10 21:39:45 +0900

    sts4 yml 및 build.gradle 셋팅

---
+++ daehan_univ/src/main/java/kr/ac/dhuniv/DaehanUnivApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.dhuniv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaehanUnivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaehanUnivApplication.class, args);
+	}
+
+}
